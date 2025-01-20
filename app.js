c1ab0cbb5f82ac17ebb35fbc7ab276c55359f1d0
pierresouza <pierre.s3@hotmail.com>
2025-01-20 17:39:02 -0300

    feat: add function to add frinds inside the list and sort random friend

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
+let amigos = [];
+let listaDeAmigos = document.getElementById('listaAmigos');
+let inputAmigos = document.getElementById('amigo');
+
+// Escutar o evento "keydown" no campo de entrada
+inputAmigos.addEventListener('keydown', (event) => {
+  if (event.key === "Enter") {
+    adicionarAmigo();
+  }
+});
+
+function adicionarAmigo() {
+  let amigo = inputAmigos.value.trim(); // Remove espaços extras
+  if (amigo) { // Verifica se o campo não está vazio
+    amigos.push(amigo); //Adiciona o amigo a lista
+    listaDeAmigos.innerHTML = amigos.join(', '); // Mostra os amigos separados por vírgula
+    inputAmigos.value = ''; // Limpa o campo de entrada
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length > 0) {
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    listaDeAmigos.innerHTML = `O amigo(a) sorteado foi: ${sorteado}`
+  } else {
+    listaDeAmigos.innerHTML = "Nenhum amigo na lista para sortear."
+  }
+}
